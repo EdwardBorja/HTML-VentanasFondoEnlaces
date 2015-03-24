c7f2a5b05917e4efc747b262509aa6c60d6bdc34
Eduardo <skate2612@hotmail.com>
2015-03-24 07:54:56 -0600

    Ventanas
    Que onda aqui les dejo este ejemplo de html usando javascript en el cual
    abrimos una ventana desde un boton,poder cambiar el color de fondo y
    color del texto,tambien poner enlaces con su icono
    -click derecho en archivo Ventanahtml.htm y abrir con "algun navegador"
    Saludos

---
+++ ventanahtml.js
@@ -0,0 +1,22 @@
+function ColorAzul(){
+      //cambia color del fondo
+    document.body.style.backgroundColor="blue"
+      //cambia color del texto
+    document.getElementById('etiqueta1').style.color = '#FFFFFF';
+}
+
+function ColorRojo(){
+    
+    document.body.style.backgroundColor="red"
+  
+    document.getElementById('etiqueta1').style.color = '#FFFFFF';
+}
+
+function ColorBlanco(){
+    
+    document.body.style.backgroundColor="white"
+    document.getElementById('etiqueta1').style.color = '#000000';
+     
+}
+
+
